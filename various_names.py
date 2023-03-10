834a5d568a1c913ee6b80c2087d6c3ef86fb4171
Zura1985 <127140212+Zura1985@users.noreply.github.com>
2023-03-10 09:36:08 -0800

    Update various_names.py

--- various_names.py
+++ various_names.py
@@ -1,7 +1,9 @@
 name = input("what is your name?")
-if name == "Ana":
+if name[0] == "A" or "B"
     print("Bonjour", name)
 elif name == "Abel":
     print("Ciao", name)
 elif name == "Zura" or "Khatia":
     print("Hello", name)
+else:
+    print("Gamarjoba", name)
