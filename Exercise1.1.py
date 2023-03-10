19b3c8c5b1d79f39d44737683e5160f45e84e9ea
Zura1985 <127140212+Zura1985@users.noreply.github.com>
2023-03-10 09:19:25 -0800

    Update Exercise1.1.py

--- Exercise1.1.py
+++ Exercise1.1.py
@@ -3,4 +3,4 @@ sakheli = input("What is your name? ").title()
 sakheli_da_gvari = sakheli.split()
 
 sakheli_final = " ".join(sakheli_da_gvari)
-print("Hello,", sakheli_final + ".", "Nice to meet you. How can I help you?" )
+print("Hello,", sakheli_final + ". Nice to meet you. How can I help you?" )
