558a386e7ed7b16504de8c094a822d46386811d1
Zura1985 <127140212+Zura1985@users.noreply.github.com>
2023-03-29 01:00:06 -0700

    Add files via upload

---
+++ Deep-7.py
@@ -0,0 +1,7 @@
+
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?").lower()
+
+if answer == "42" or answer == "forty-two" or answer == "Forty two":
+    print("Yes")
+else:
+    print("No")
